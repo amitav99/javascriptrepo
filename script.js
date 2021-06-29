b08ce6eaab693253e544305e5ca39bb96b9ca6c6
Amitav <amitavdebnath2000@gmail.com>
2021-06-30 00:19:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("hello world");
+// var name = "amitav debnath"
+// console.log("Hi there")
+// console.warn("Be careful")
+// console.error("I ts wrong")
+// console.log(name)
+
+// var name ="amitav"; 
+// if(name=="amitav");{
+//     let apples =5;
+//     var height ="6.0\"";
+//     console.log("i have",apples ,"apples")
+//    
+function addnumbers (num1, num2)
+{
+    const total= num2+num2;
+    return total;
+}
+const addedtotal = addnumbers(4, 4)
