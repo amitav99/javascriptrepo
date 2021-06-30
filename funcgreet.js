a62306f83b17fbf30a9f281ef650280bd702fc45
Amitav <amitavdebnath2000@gmail.com>
2021-07-01 03:16:45 +0600

    new commit

---
+++ funcgreet.js
@@ -0,0 +1,22 @@
+// function addition(name, ...numbers) {
+//   let total = 0;
+//   for (i in numbers) {
+//     total = total + numbers[i];
+//   }
+//   return `${name}: The total is ${total}`;
+// }
+// const  all_total = addition ("amitav", 2, 3);
+// console.log(all_total);
+ 
+// const person1 = {
+//   name: "amitav",
+//   age: 21,
+//   height: "6'0\"",
+// };
+
+// const person2 = {
+//   name: "ankur",
+//   age: 32,
+//   occupation: "software engineer",
+// };
+// console.log(person2["occupation"]);
--- function.js
+++ function.js
@@ -2,4 +2,4 @@ function pro (n1, n2){
     const tot =n1*n2;
     return tot;
 }
-const prototal= pro(5, 2)
+const prototal= pro(5, 2) 
